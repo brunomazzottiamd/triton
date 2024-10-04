738db70ee5f3e3e3aa0399ee99a393f2b3bb685b
Bruno Mazzotti <bruno.mazzotti@amd.com>
2024-10-04 17:42:18 +0000

    Improve Triton tracing script
    * Copy TTIR, TTGIR and assembly files from kernel.
    * Compress to XZ format.
    * Remove raw ATT files and keep only the parsed ATT files.

--- 283_int8_gemm_perf_investigation/trace_triton.sh
+++ 283_int8_gemm_perf_investigation/trace_triton.sh
@@ -13,14 +13,28 @@ remove() {
     rm --recursive --force "${@}"
 }
 
+copy_kernel_file() {
+    kernel_file_desc="${1}"
+    kernel_file_ext="${2}"
+    triton_cache_dir="${3}"
+    output_dir="${4}"
+    echo "Getting kernel ${kernel_file_desc}..."
+    kernel_file=$(find "${triton_cache_dir}" -name "*.${kernel_file_ext}" | head -1)
+    echo "Kernel ${kernel_file_desc} is [${kernel_file}]."
+    cp "${kernel_file}" "${output_dir}"
+}
+
 
 ### Start tracing script
+
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+kernel_source="${script_dir}/test_int8_gemm.py"
 
 M=20
 N=1920
 K=13312
 
-echo "TRACING TRITON KERNEL FOR (M, N, K) = (${M}, ${N}, ${K})"
+echo "TRACING TRITON KERNEL [${kernel_source}] FOR (M, N, K) = (${M}, ${N}, ${K})"
 
 
 ### Set output directory
@@ -28,10 +42,10 @@ echo "TRACING TRITON KERNEL FOR (M, N, K) = (${M}, ${N}, ${K})"
 output_dir=$(trim_string "${1}")
 if [ -z "${output_dir}" ]; then
     # "${1}" is empty, use a sensible default as output directory.
-    output_dir=$(date '+results_%Y-%m-%dT%H:%M:%S')
+    output_dir=$(date '+results_%Y-%m-%dT%H-%M-%S')
 fi
 
-output_zip="$(basename "${output_dir}").zip"
+output_zip="$(basename "${output_dir}").tar.xz"
 
 echo "Output directory is [${output_dir}]. It'll be compressed to [${output_zip}]."
 
@@ -63,10 +77,8 @@ mkdir --parents "${output_dir}"
 
 echo 'Getting kernel dispatch ID...'
 
-script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-
 kernel_program=(
-    python "${script_dir}/test_int8_gemm.py" run -M "${M}" -N "${N}" -K "${K}"
+    python "${kernel_source}" run -M "${M}" -N "${N}" -K "${K}"
 )
 
 dispatch_id=$(rocprofv2 \
@@ -77,6 +89,16 @@ dispatch_id=$(rocprofv2 \
 )
 
 echo "Kernel dispatch ID is ${dispatch_id}."
+
+
+### Get kernel IRs and assembly code
+
+copy_kernel_file 'Triton IR' 'ttir' "${triton_cache_dir}" "${output_dir}"
+copy_kernel_file 'Triton GPU IR' 'ttgir' "${triton_cache_dir}" "${output_dir}"
+copy_kernel_file 'assembly' 'amdgcn' "${triton_cache_dir}" "${output_dir}"
+
+# Copy reference Python code too.
+cp "${kernel_source}" "${output_dir}"
 
 
 ### Create rocprofv2 input file
@@ -108,18 +130,17 @@ rocprofv2 \
     --output-directory "${output_dir}" \
     "${kernel_program[@]}"
 
-remove "${output_dir}"/*.out
+# Remove large files, keep only the parsed ATT.
+remove "${output_dir}"/*.out "${output_dir}"/*.att "${output_dir}"/*.txt
 
 ### Compress output directory
 # It's easier to transfer a single zip file!
 
 echo "Compressing output directory to [${output_zip}]..."
 
-zip \
-    -q \
-    -9 \
-    -r \
-    "${output_zip}" \
+tar \
+    -cf "${output_zip}" \
+    -I 'xz -9e' \
     "${output_dir}"
 
 
