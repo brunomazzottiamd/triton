2e3a98d94abce889dd5b4ac69c003483f79215d8
Bruno Mazzotti <bruno.mazzotti@amd.com>
2024-10-09 20:36:15 +0000

    Try to update solution index and hipBLASLt tracer
    Not successful!

--- 283_int8_gemm_perf_investigation/trace_hipblaslt.sh
+++ 283_int8_gemm_perf_investigation/trace_hipblaslt.sh
@@ -22,18 +22,16 @@ K=13312
 
 echo "TRACING HIPBLASLT KERNEL FOR (M, N, K) = (${M}, ${N}, ${K})"
 
-cd /root/hipBLASLt/build/release/clients/staging || exit 1
-
 
 ### Set output directory
 
 output_dir=$(trim_string "${1}")
 if [ -z "${output_dir}" ]; then
     # "${1}" is empty, use a sensible default as output directory.
-    output_dir=$(date '+results_%Y-%m-%dT%H:%M:%S')
+    output_dir=$(date '+results_%Y-%m-%dT%H-%M-%S')
 fi
 
-output_zip="$(basename "${output_dir}").zip"
+output_zip="$(basename "${output_dir}").tar.zx"
 
 echo "Output directory is [${output_dir}]. It'll be compressed to [${output_zip}]."
 
@@ -49,15 +47,19 @@ remove "${output_dir}" "${output_zip}"
 
 echo 'Getting kernel dispatch ID...'
 
-# HIP_FORCE_DEV_KERNARG=1 ./hipblaslt-bench \
-#     --function matmul \
-#     -m "${m}" -n "${n}" -k "${k}" \
-#     --transA T --transB N \
-#     --precision i8_r --compute_type i32_r \
-#     --cold_iters 0 --iters 1 \
-#     --api_method cpp --algo_method index --solution_index 67826
-#     --print_kernel_info
+# Solution index can be found in "hipblaslt_bench_results/${M}_${N}_${K}_winner.txt" file.
+solution_index=99867
+# solution_index=613
+HIP_FORCE_DEV_KERNARG=1 hipblaslt-bench \
+    --function matmul \
+    -m "${M}" -n "${N}" -k "${K}" \
+    --transA T --transB N \
+    --precision i8_r --compute_type i32_r \
+    --cold_iters 0 --iters 1 \
+    --api_method cpp --algo_method index --solution_index "${solution_index}" \
+    --print_kernel_info
 
-# HIP_FORCE_DEV_KERNARG=1 ./hipblaslt-bench -f matmul -r f16_r -m 4864 -n 4096 -k 8192 --transA T \
-# --transB N --compute_type f32_r --api_method cpp --algo_method index --solution_index 67826 -i 10 \
-# -j 100 --print_kernel_info
+# Getting this error:
+# error: NO solution found!
+
+exit 0
