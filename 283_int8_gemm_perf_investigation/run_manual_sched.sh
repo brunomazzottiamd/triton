0ccac77a7ab1be0d3dd6ffbfc1a4b1f0b80f1b67
Bruno Mazzotti <bruno.mazzotti@amd.com>
2024-10-31 13:27:05 +0000

    Compute kernel execution time with rocprof

--- 283_int8_gemm_perf_investigation/run_manual_sched.sh
+++ 283_int8_gemm_perf_investigation/run_manual_sched.sh
@@ -17,11 +17,11 @@ copy_kernel_file() {
     kernel_file_desc="${1}"
     kernel_file_ext="${2}"
     triton_cache_dir="${3}"
-    output_dir="${4}"
+    output_file="${4}"
     echo "Getting kernel ${kernel_file_desc}..."
     kernel_file=$(find "${triton_cache_dir}" -name "*.${kernel_file_ext}" | head -1)
     echo "Kernel ${kernel_file_desc} is [${kernel_file}]."
-    cp "${kernel_file}" "${output_dir}"
+    cp "${kernel_file}" "${output_file}"
 }
 
 TRITON_CACHE_DIR="${HOME}/.triton/cache"
@@ -31,6 +31,20 @@ clean_triton_cache() {
     remove "${TRITON_CACHE_DIR}"
 }
 
+KERNEL_NAME='_triton_gemm_a8w8_kernel_no_autotune'
+
+get_kernel_time_ns() {
+    stats_file="${1}"
+    grep "${KERNEL_NAME}" "${stats_file}" | cut --delimiter ',' --fields 3
+}
+
+get_kernel_time_us() {
+    stats_file="${1}"
+    echo "scale=2; ($(get_kernel_time_ns "${stats_file}") / 1000) + 0.05" | bc --mathlib
+}
+
+
+### ENTRY POINT
 
 echo 'TESTING TRITON KERNEL WITH MANUAL SCHEDULING...'
 
@@ -60,9 +74,9 @@ echo "Creating new empty output directory [${output_dir}]..."
 mkdir --parents "${output_dir}"
 
 
-### Run Triton kernel without assembly injection
+### Run reference Triton kernel
 
-echo 'Running kernel to get reference assembly...'
+echo 'Running reference kernel to get baseline assembly and baseline performance...'
 
 clean_triton_cache
 
@@ -78,24 +92,44 @@ kernel_program=(
 )
 
 unset AMD_INSERT_AMDGCN
-"${kernel_program[@]}"
+rocprof --stats -o "${output_dir}/prof_results_ref.csv" "${kernel_program[@]}"
 
-copy_kernel_file 'assembly' 'amdgcn' "${TRITON_CACHE_DIR}" "${output_dir}"
+ref_time=$(get_kernel_time_us "${output_dir}/prof_results_ref.stats.csv")
+echo "Reference time is ${ref_time} us."
+
+asm_ref_file="${output_dir}/asm_ref.amdgcn"
+copy_kernel_file 'assembly' 'amdgcn' "${TRITON_CACHE_DIR}" "${asm_ref_file}"
+
+pytest "${kernel_source}"
+python "${kernel_source}" bench
 
 
-### Run Triton kernel with assembly injection
+### Run Triton kernel with manual schedule assembly injection
 
-echo 'Running kernel with assembly injection...'
+echo 'Running kernel with manual schedule assembly injection...'
 
 clean_triton_cache
 
 export AMD_INSERT_AMDGCN="${script_dir}/my_asm.amdgcn"
+rocprof --stats -o "${output_dir}/prof_results_msched.csv" "${kernel_program[@]}"
 
-pytest -vvv "${kernel_source}" \
-    && python "${kernel_source}" bench \
-    && diff --report-identical-files "${output_dir}"/*.amdgcn "${AMD_INSERT_AMDGCN}" > "${output_dir}/asm.diff"
+msched_time=$(get_kernel_time_us "${output_dir}/prof_results_msched.stats.csv")
+echo "Manual schedule time is ${msched_time} us."
+
+asm_msched_file="${output_dir}/asm_msched.amdgcn"
+cp "${AMD_INSERT_AMDGCN}" "${asm_msched_file}"
+
+pytest "${kernel_source}"
+python "${kernel_source}" bench
 
 unset AMD_INSERT_AMDGCN
+
+
+### Report differences
+
+echo 'Reporting code and performance differences...'
+
+diff --report-identical-files "${asm_ref_file}" "${asm_msched_file}" > "${output_dir}/asm.diff"
 
 
 ### Done
