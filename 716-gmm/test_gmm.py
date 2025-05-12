f8d16a973f83d363e576150b3980712364d7a5a8
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-05-12 18:17:31 -0300

    [WIP] implement matrix transposition

--- 716-gmm/test_gmm.py
+++ 716-gmm/test_gmm.py
@@ -12,7 +12,15 @@ import torch
 import pytest
 
 # Common module
-from common import DEVICE, SUPPORTED_DTYPES_STR, TILING, dtype_from_str, gen_input
+from common import (
+    DEVICE,
+    SUPPORTED_DTYPES_STR,
+    TILING,
+    dtype_from_str,
+    gen_input,
+    gen_multiple_group_sizes,
+    gen_output,
+)
 
 # GMM implementations
 from torch_gmm import torch_gmm
@@ -107,12 +115,28 @@ QUICK_TEST: bool = False
 @pytest.mark.parametrize(
     "out_dtype_str", {"o" + dtype_str for dtype_str in SUPPORTED_DTYPES_STR}
 )
-@pytest.mark.parametrize("rng_seed", [0, 77, 121])
+@pytest.mark.parametrize("trans_lhs_str", {f"tlhs{b}" for b in {"F", "T"}})
+@pytest.mark.parametrize("trans_rhs_str", {f"trhs{b}" for b in {"F", "T"}})
+@pytest.mark.parametrize("trans_out_str", {f"tout{b}" for b in {"F", "T"}})
+@pytest.mark.parametrize("rng_seed_str", {f"rng{rng_seed}" for rng_seed in {77, 121}})
 def test_gmm(
-    M: int, K: int, N: int, G: int, in_dtype_str: str, out_dtype_str: str, rng_seed: int
+    M: int,
+    K: int,
+    N: int,
+    G: int,
+    in_dtype_str: str,
+    out_dtype_str: str,
+    trans_lhs_str: str,
+    trans_rhs_str: str,
+    trans_out_str: str,
+    rng_seed_str: str,
 ):
     in_dtype = dtype_from_str(in_dtype_str)
     out_dtype = dtype_from_str(out_dtype_str)
+    trans_lhs = trans_lhs_str.replace("tlhs", "") == "T"
+    trans_rhs = trans_rhs_str.replace("trhs", "") == "T"
+    trans_out = trans_out_str.replace("tout", "") == "T"
+    rng_seed = int(rng_seed_str.replace("rng", ""))
 
     # Skip conditions:
     if in_dtype == torch.float32:
@@ -124,20 +148,44 @@ def test_gmm(
         pytest.skip("Skipping mixed fp16 / bf16 types to speed up test execution.")
         # Important notice: mixed fp16 / bf16 types work correctly!
 
-    lhs, rhs, group_sizes = gen_input(
-        M, K, N, G, preferred_element_type=in_dtype, rng_seed=rng_seed
+    lhs, rhs, group_sizes_0 = gen_input(
+        M,
+        K,
+        N,
+        G,
+        preferred_element_type=in_dtype,
+        trans_lhs=trans_lhs,
+        trans_rhs=trans_rhs,
+        rng_seed=rng_seed,
+    )
+    # Reduce number of distinct group sizes in quick test.
+    num_group_sizes = 2 if QUICK_TEST else 5
+    multiple_group_sizes = gen_multiple_group_sizes(
+        num_group_sizes, M, G, rng_seed=None, group_sizes_0=group_sizes_0
     )
 
-    out_torch = torch_gmm(lhs, rhs, group_sizes, preferred_element_type=out_dtype)
+    out_torch = gen_output(M, N, preferred_element_type=out_dtype, trans=trans_out)
+    out_triton = gen_output(M, N, preferred_element_type=out_dtype, trans=trans_out)
 
     # Don't use autotune for test only shapes, don't use autotune in quick test.
     tiling = TILING if (M, K, N, G) in TEST_ONLY_SHAPES or QUICK_TEST else None
-    out_triton = triton_gmm(
-        lhs,
-        rhs,
-        group_sizes,
-        preferred_element_type=out_dtype,
-        tiling=tiling,
-    )
 
-    torch.testing.assert_close(out_torch, out_triton, atol=5e-3, rtol=1e-2)
+    for group_sizes in multiple_group_sizes:
+        torch_gmm(
+            lhs,
+            rhs,
+            group_sizes,
+            preferred_element_type=out_dtype,
+            existing_out=out_torch,
+        )
+
+        triton_gmm(
+            lhs,
+            rhs,
+            group_sizes,
+            preferred_element_type=out_dtype,
+            existing_out=out_triton,
+            tiling=tiling,
+        )
+
+        torch.testing.assert_close(out_torch, out_triton, atol=5e-3, rtol=1e-2)
