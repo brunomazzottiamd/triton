4d9fa19b0a351e49f4cda1dfeeed1db3e420faaa
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-05-13 19:56:24 +0000

    benchmark all lhs and rhs GEMM layouts

--- 716-gmm/bench_gmm.sh
+++ 716-gmm/bench_gmm.sh
@@ -1,3 +1,27 @@
 #!/usr/bin/env bash
 
-python gmm.py --bench --verbose --num-group-sizes 20
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+function bench() {
+    python "${script_dir}/gmm.py" --bench --verbose --num-group-sizes 20 "${@}" 2>&1
+}
+
+rm --recursive --force "${script_dir}"/*.log
+
+# row-major x row-major
+bench --no-trans-rhs | tee "${script_dir}/bench_rr.log"
+
+# row-major x column-major
+bench | tee "${script_dir}/bench_rc.log"
+
+# column-major x row-major
+bench --trans-lhs --no-trans-rhs | tee "${script_dir}/bench_cr.log"
+
+# column-major x column-major
+bench --trans-lhs | tee "${script_dir}/bench_cc.log"
+
+grep best_config "${script_dir}/bench_"*.log \
+    | tr --squeeze-repeats ' ' \
+    | cut --delimiter ' ' --fields 6- \
+    | sort \
+    | uniq > "${script_dir}/best_configs.log"
