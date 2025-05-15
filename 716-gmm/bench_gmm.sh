afa4709f85705d7643f72c0abb2d2fc8f02aa6cf
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-05-15 18:44:55 +0000

    improve bench script
    * add messages with timestamps
    * log torch and triton versions
    * prune transposition configurations (TN for inference, TN+NN+NT
      for training)

--- 716-gmm/bench_gmm.sh
+++ 716-gmm/bench_gmm.sh
@@ -1,47 +1,62 @@
 #!/usr/bin/env bash
 
+
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+
+function log() {
+    timestamp=$(date +'%Y-%m-%d %H:%M:%S,%3N')
+    echo "${timestamp} > ${*}"
+}
+
 
 function bench() {
     python "${script_dir}/gmm.py" --bench --verbose --num-group-sizes 20 "${@}" 2>&1
 }
 
-rm --recursive --force "${script_dir}"/*.log
 
-# row-major x row-major => row-major
-bench --no-trans-rhs \
-    | tee "${script_dir}/bench_rrr.log"
+function main() {
+    workload='inference'
+    # workload='training'
+    log "Benchmarking for ${workload} workload..."
 
-# row-major x row-major => column-major
-bench --no-trans-rhs --trans-out \
-    | tee "${script_dir}/bench_rrc.log"
+    log 'Removing old benchmark log files...'
+    rm --recursive --force "${script_dir}"/*.log
 
-# row-major x column-major => row-major
-bench \
-    | tee "${script_dir}/bench_rcr.log"
+    export PYTHONWARNINGS='ignore'
+    torch_version=$(pip show torch | grep Version | cut --delimiter ' ' --field 2)
+    triton_version=$(pip show triton | grep Version | cut --delimiter ' ' --field 2)
+    log "PyTorch version = ${torch_version}" | tee "${script_dir}/version.log"
+    log "Triton version = ${triton_version}" | tee --append "${script_dir}/version.log"
 
-# row-major x column-major => column-major
-bench --trans-out \
-    | tee "${script_dir}/bench_rcc.log"
+    # inference layouts:
+    # * TN: row-major x column-major
 
-# column-major x row-major => row-major
-bench --trans-lhs --no-trans-rhs \
-    | tee "${script_dir}/bench_crr.log"
+    # training layouts:
+    # * TN: row-major x column-major
+    # * NN: column-major x column-major
+    # * NT: column-major x row-major
 
-# column-major x row-major => column-major
-bench --trans-lhs --no-trans-rhs --trans-out \
-    | tee "${script_dir}/bench_crc.log"
+    # TN: row-major x column-major => row-major
+    bench \
+	| tee "${script_dir}/bench_rcr.log"
 
-# column-major x column-major => row-major
-bench --trans-lhs \
-    | tee "${script_dir}/bench_ccr.log"
+    if [ "${workload}" == 'training' ]; then
+	# NN: column-major x column-major => row-major
+	bench --trans-lhs \
+	    | tee "${script_dir}/bench_ccr.log"
 
-# column-major x column-major => column-major
-bench --trans-lhs --trans-out \
-    | tee "${script_dir}/bench_ccc.log"
+	# NT: column-major x row-major => row-major
+	bench --trans-lhs --no-trans-rhs \
+	    | tee "${script_dir}/bench_crr.log"
+    fi
 
-grep best_config "${script_dir}/bench_"*.log \
-    | tr --squeeze-repeats ' ' \
-    | cut --delimiter ' ' --fields 6- \
-    | sort \
-    | uniq > "${script_dir}/best_configs.log"
+    grep best_config "${script_dir}/bench_"*.log \
+	| tr --squeeze-repeats ' ' \
+	| cut --delimiter ' ' --fields 6- \
+	| sort \
+	| uniq > "${script_dir}/best_configs.log"
+}
+
+
+main
