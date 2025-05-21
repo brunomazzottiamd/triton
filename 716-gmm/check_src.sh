57f68c97173e26a5e4066cc89a2790fa9b08ace7
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-05-21 09:21:00 -0300

    ignore shellcheck literal carriage return warnings on Windows

--- 716-gmm/check_src.sh
+++ 716-gmm/check_src.sh
@@ -4,9 +4,15 @@ script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 black "${script_dir}"/*.py
 ruff check "${script_dir}"/*.py
-# mypy has issues with Tirton on Windows...
+
+# mypy has issues with Triton on Windows, run it only on Linux.
 if [ "$(uname)" == Linux ]; then
     mypy --ignore-missing-imports "${script_dir}"/*.py
 fi
 
-shellcheck "${script_dir}"/*.sh
+if [ "$(uname)" == Linux ]; then
+    shellcheck "${script_dir}"/*.sh
+else
+    # Ignore literal carriage return warnings on Windows.
+    shellcheck --exclude SC1017 "${script_dir}"/*.sh
+fi
