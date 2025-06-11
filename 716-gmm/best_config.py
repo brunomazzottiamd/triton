96b3230ce0e724dea56c6d0da1960e8481ab26af
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-06-11 16:30:55 -0300

    add one more best config for ptgmm

--- 716-gmm/best_config.py
+++ 716-gmm/best_config.py
@@ -138,7 +138,7 @@ BEST_PERSISTENT_TGMM_CONFIGS: dict[ConfigKey, Config] = {
     ConfigKey(M=  49152, K= 1408, N= 2048, G=64): Config(block_size_m=32, block_size_k=128, block_size_n=256, group_size=4, grid_dim=304, num_warps=8, num_stages=2),
     ConfigKey(M=3145728, K= 2048, N= 1408, G= 8): Config(block_size_m=64, block_size_k=128, block_size_n=256, group_size=4, grid_dim=912, num_warps=4, num_stages=1),
     ConfigKey(M= 393216, K= 2048, N= 1408, G=64): Config(block_size_m=32, block_size_k= 64, block_size_n=256, group_size=8, grid_dim=304, num_warps=8, num_stages=2),
-    ConfigKey(M=  32768, K= 6144, N=16384, G= 8): DEFAULT_PERSISTENT_TGMM_CONFIG,
+    ConfigKey(M=  32768, K= 6144, N=16384, G= 8): Config(block_size_m=32, block_size_k=128, block_size_n=256, group_size=4, grid_dim=304, num_warps=8, num_stages=2),
     ConfigKey(M=  32768, K=16384, N= 6144, G= 8): DEFAULT_PERSISTENT_TGMM_CONFIG,
 }
 # fmt: on
