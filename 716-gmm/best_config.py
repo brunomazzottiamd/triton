8a45237c2c09483f53b8ce6e84ab6ee3d12e075d
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-06-13 11:04:33 -0300

    comment out some best configs to speedup autotuning
    target layouts are:
    * NN GMM
    * NT GMM
    * NN TGMM
    * TN TGMM

--- 716-gmm/best_config.py
+++ 716-gmm/best_config.py
@@ -136,11 +136,11 @@ BEST_GMM_CONFIGS: dict[ConfigKey, Config] = {
     ConfigKey(M=  32768, K= 6144, N=16384, G= 8): Config(block_size_m=256, block_size_k=32, block_size_n=256, group_size=4, grid_dim= 608, num_warps=8, num_stages=2),
     ConfigKey(M=  32768, K=16384, N= 6144, G= 8): Config(block_size_m=256, block_size_k=32, block_size_n=256, group_size=4, grid_dim= 304, num_warps=8, num_stages=2),
     # crr (TN) - incomplete tuning, it's not a target layout anymore
-    ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_lhs=True): Config(block_size_m=256, block_size_k=32, block_size_n= 64, group_size=1, num_warps=8, num_stages=2, grid_dim=608),
-    ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_lhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=8, num_warps=8, num_stages=1),
-    ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_lhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=8, num_warps=4, num_stages=1),
-    ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=2, num_warps=4, num_stages=1),
-    ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=8, num_warps=8, num_stages=2),
+  # ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_lhs=True): Config(block_size_m=256, block_size_k=32, block_size_n= 64, group_size=1, num_warps=8, num_stages=2, grid_dim=608),
+  # ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_lhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=8, num_warps=8, num_stages=1),
+  # ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_lhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=8, num_warps=4, num_stages=1),
+  # ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=2, num_warps=4, num_stages=1),
+  # ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=8, num_warps=8, num_stages=2),
     # rcr (NT) - TODO: incomplete tuning, retune to get best grid_dim!
     ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_rhs=True): Config(block_size_m= 64, block_size_k=32, block_size_n=256, group_size=4, num_warps=8, num_stages=2, grid_dim=608),
     ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_rhs=True): Config(block_size_m=128, block_size_k=32, block_size_n=256, group_size=8, num_warps=8, num_stages=1, grid_dim=304),
@@ -148,11 +148,11 @@ BEST_GMM_CONFIGS: dict[ConfigKey, Config] = {
     ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_rhs=True): Config(block_size_m=128, block_size_k=32, block_size_n=256, group_size=2, num_warps=8, num_stages=1),
     ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_rhs=True): Config(block_size_m=128, block_size_k=32, block_size_n=256, group_size=2, num_warps=8, num_stages=1),
     # ccr - incomplete tuning, it's not a target layout anymore
-    ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_lhs=True, trans_rhs=True): Config(block_size_m=128, block_size_k=32, block_size_n=256, group_size=4, num_warps=4, num_stages=1, grid_dim=912),
-    ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_lhs=True, trans_rhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=4, num_warps=8, num_stages=1),
-    ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_lhs=True, trans_rhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=8, num_warps=4, num_stages=1),
-    ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True, trans_rhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=8, num_warps=4, num_stages=1),
-    ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True, trans_rhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=2, num_warps=4, num_stages=1),
+  # ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_lhs=True, trans_rhs=True): Config(block_size_m=128, block_size_k=32, block_size_n=256, group_size=4, num_warps=4, num_stages=1, grid_dim=912),
+  # ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_lhs=True, trans_rhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=4, num_warps=8, num_stages=1),
+  # ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_lhs=True, trans_rhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=8, num_warps=4, num_stages=1),
+  # ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True, trans_rhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=8, num_warps=4, num_stages=1),
+  # ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True, trans_rhs=True): Config(block_size_m=256, block_size_k=32, block_size_n=128, group_size=2, num_warps=4, num_stages=1),
 }
 # fmt: on
 
@@ -173,17 +173,17 @@ BEST_PERSISTENT_TGMM_CONFIGS: dict[ConfigKey, Config] = {
     ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True): Config(block_size_m=32, block_size_k=256, block_size_n=128, group_size=2, grid_dim= 608, num_warps=8, num_stages=1),
     ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True): Config(block_size_m=32, block_size_k=256, block_size_n=128, group_size=2, grid_dim= 304, num_warps=8, num_stages=1),
     # rcr (NT) - incomplete tuning, it's not a target layout anymore
-    ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_rhs=True): Config(block_size_m=32, block_size_k=64, block_size_n=256, group_size=2, grid_dim=1216, num_warps=4, num_stages=2),
-    ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_rhs=True): DEFAULT_PERSISTENT_TGMM_CONFIG,
-    ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_rhs=True): Config(block_size_m=64, block_size_k=64, block_size_n=256, group_size=4, grid_dim= 912, num_warps=4, num_stages=1),
-    ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_rhs=True): Config(block_size_m=64, block_size_k=64, block_size_n=256, group_size=1, grid_dim= 304, num_warps=4, num_stages=2),
-    ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_rhs=True): Config(block_size_m=32, block_size_k=32, block_size_n=256, group_size=1, grid_dim= 608, num_warps=8, num_stages=1),
+  # ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_rhs=True): Config(block_size_m=32, block_size_k=64, block_size_n=256, group_size=2, grid_dim=1216, num_warps=4, num_stages=2),
+  # ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_rhs=True): DEFAULT_PERSISTENT_TGMM_CONFIG,
+  # ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_rhs=True): Config(block_size_m=64, block_size_k=64, block_size_n=256, group_size=4, grid_dim= 912, num_warps=4, num_stages=1),
+  # ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_rhs=True): Config(block_size_m=64, block_size_k=64, block_size_n=256, group_size=1, grid_dim= 304, num_warps=4, num_stages=2),
+  # ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_rhs=True): Config(block_size_m=32, block_size_k=32, block_size_n=256, group_size=1, grid_dim= 608, num_warps=8, num_stages=1),
     # ccr - incomplete tuning, it's not a target layout anymore
-    ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_lhs=True, trans_rhs=True): Config(block_size_m=32, block_size_k=256, block_size_n=128, group_size=2, grid_dim= 912, num_warps=4, num_stages=1),
-    ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_lhs=True, trans_rhs=True): DEFAULT_PERSISTENT_TGMM_CONFIG,
-    ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_lhs=True, trans_rhs=True): Config(block_size_m=64, block_size_k=256, block_size_n= 64, group_size=8, grid_dim=1216, num_warps=8, num_stages=1),
-    ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True, trans_rhs=True): Config(block_size_m=32, block_size_k=128, block_size_n=256, group_size=1, grid_dim= 304, num_warps=8, num_stages=1),
-    ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True, trans_rhs=True): Config(block_size_m=32, block_size_k=256, block_size_n=128, group_size=2, grid_dim= 304, num_warps=8, num_stages=1),
+  # ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_lhs=True, trans_rhs=True): Config(block_size_m=32, block_size_k=256, block_size_n=128, group_size=2, grid_dim= 912, num_warps=4, num_stages=1),
+  # ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_lhs=True, trans_rhs=True): DEFAULT_PERSISTENT_TGMM_CONFIG,
+  # ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_lhs=True, trans_rhs=True): Config(block_size_m=64, block_size_k=256, block_size_n= 64, group_size=8, grid_dim=1216, num_warps=8, num_stages=1),
+  # ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True, trans_rhs=True): Config(block_size_m=32, block_size_k=128, block_size_n=256, group_size=1, grid_dim= 304, num_warps=8, num_stages=1),
+  # ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True, trans_rhs=True): Config(block_size_m=32, block_size_k=256, block_size_n=128, group_size=2, grid_dim= 304, num_warps=8, num_stages=1),
 }
 # fmt: on
 
@@ -204,17 +204,17 @@ BEST_NON_PERSISTENT_TGMM_CONFIGS: dict[ConfigKey, Config] = {
     ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True): Config(grid_dim=None, block_size_m=32, block_size_k=256, block_size_n=256, group_size=8, num_warps=8, num_stages=1),
     ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True): Config(grid_dim=None, block_size_m=32, block_size_k=256, block_size_n=256, group_size=8, num_warps=8, num_stages=1),
     # rcr (NT) - it's not a target layout anymore
-    ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_rhs=True): Config(grid_dim=None, block_size_m=32, block_size_k=128, block_size_n=256, group_size=1, num_warps=8, num_stages=2),
-    ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_rhs=True): Config(grid_dim=None, block_size_m=64, block_size_k= 32, block_size_n=256, group_size=8, num_warps=8, num_stages=1),
-    ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_rhs=True): Config(grid_dim=None, block_size_m=64, block_size_k= 64, block_size_n=256, group_size=1, num_warps=4, num_stages=1),
-    ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_rhs=True): Config(grid_dim=None, block_size_m=32, block_size_k= 64, block_size_n=256, group_size=8, num_warps=4, num_stages=2),
-    ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_rhs=True): Config(grid_dim=None, block_size_m=32, block_size_k= 64, block_size_n=256, group_size=8, num_warps=4, num_stages=2),
+  # ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_rhs=True): Config(grid_dim=None, block_size_m=32, block_size_k=128, block_size_n=256, group_size=1, num_warps=8, num_stages=2),
+  # ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_rhs=True): Config(grid_dim=None, block_size_m=64, block_size_k= 32, block_size_n=256, group_size=8, num_warps=8, num_stages=1),
+  # ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_rhs=True): Config(grid_dim=None, block_size_m=64, block_size_k= 64, block_size_n=256, group_size=1, num_warps=4, num_stages=1),
+  # ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_rhs=True): Config(grid_dim=None, block_size_m=32, block_size_k= 64, block_size_n=256, group_size=8, num_warps=4, num_stages=2),
+  # ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_rhs=True): Config(grid_dim=None, block_size_m=32, block_size_k= 64, block_size_n=256, group_size=8, num_warps=4, num_stages=2),
     # ccr - it's not a target layout anymore
-    ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_lhs=True, trans_rhs=True): Config(grid_dim=None, block_size_m=32, block_size_k=128, block_size_n=128, group_size=1, num_warps=8, num_stages=1),
-    ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_lhs=True, trans_rhs=True): Config(grid_dim=None, block_size_m=64, block_size_k=256, block_size_n=128, group_size=2, num_warps=4, num_stages=1),
-    ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_lhs=True, trans_rhs=True): Config(grid_dim=None, block_size_m=64, block_size_k=256, block_size_n=128, group_size=1, num_warps=8, num_stages=1),
-    ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True, trans_rhs=True): Config(grid_dim=None, block_size_m=32, block_size_k=128, block_size_n=128, group_size=8, num_warps=8, num_stages=1),
-    ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True, trans_rhs=True): Config(grid_dim=None, block_size_m=32, block_size_k=128, block_size_n=128, group_size=8, num_warps=8, num_stages=1),
+  # ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_lhs=True, trans_rhs=True): Config(grid_dim=None, block_size_m=32, block_size_k=128, block_size_n=128, group_size=1, num_warps=8, num_stages=1),
+  # ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_lhs=True, trans_rhs=True): Config(grid_dim=None, block_size_m=64, block_size_k=256, block_size_n=128, group_size=2, num_warps=4, num_stages=1),
+  # ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_lhs=True, trans_rhs=True): Config(grid_dim=None, block_size_m=64, block_size_k=256, block_size_n=128, group_size=1, num_warps=8, num_stages=1),
+  # ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True, trans_rhs=True): Config(grid_dim=None, block_size_m=32, block_size_k=128, block_size_n=128, group_size=8, num_warps=8, num_stages=1),
+  # ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True, trans_rhs=True): Config(grid_dim=None, block_size_m=32, block_size_k=128, block_size_n=128, group_size=8, num_warps=8, num_stages=1),
 }
 # fmt: on
 
