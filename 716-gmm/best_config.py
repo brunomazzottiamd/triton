67c00a8a90473f98bde9b722d33a1be1d3f55ea9
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-06-16 10:08:41 -0300

    add tuned best configs: nt gmm + tn tgmm

--- 716-gmm/best_config.py
+++ 716-gmm/best_config.py
@@ -134,12 +134,11 @@ BEST_GMM_CONFIGS: dict[ConfigKey, Config] = {
     ConfigKey(M=  32768, K= 6144, N=16384, G= 8): Config(block_size_m=256, block_size_k=32, block_size_n=256, group_size=4, grid_dim= 608, num_warps=8, num_stages=2),
     ConfigKey(M=  32768, K=16384, N= 6144, G= 8): Config(block_size_m=256, block_size_k=32, block_size_n=256, group_size=4, grid_dim= 304, num_warps=8, num_stages=2),
     # NT
-    # TODO: Tune NT GMM.
-    ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_rhs=True): DEFAULT_GMM_CONFIG,
-    ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_rhs=True): DEFAULT_GMM_CONFIG,
-    ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_rhs=True): DEFAULT_GMM_CONFIG,
-    ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_rhs=True): DEFAULT_GMM_CONFIG,
-    ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_rhs=True): DEFAULT_GMM_CONFIG,
+    ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_rhs=True): Config(block_size_m=128, block_size_k= 64, block_size_n=256, group_size=4, grid_dim= 304, num_warps=8, num_stages=2),
+    ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_rhs=True): Config(block_size_m=256, block_size_k= 64, block_size_n=256, group_size=2, grid_dim= 912, num_warps=8, num_stages=2),
+    ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_rhs=True): Config(block_size_m=128, block_size_k=128, block_size_n=128, group_size=4, grid_dim= 304, num_warps=8, num_stages=2),
+    ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_rhs=True): Config(block_size_m=256, block_size_k= 64, block_size_n=256, group_size=8, grid_dim=1216, num_warps=8, num_stages=2),
+    ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_rhs=True): Config(block_size_m=256, block_size_k= 64, block_size_n=256, group_size=4, grid_dim=1216, num_warps=8, num_stages=2),
 }
 # fmt: on
 
@@ -154,12 +153,11 @@ BEST_PERSISTENT_TGMM_CONFIGS: dict[ConfigKey, Config] = {
     ConfigKey(M=  32768, K= 6144, N=16384, G= 8): Config(block_size_m=32, block_size_k=128, block_size_n=256, group_size=4, grid_dim=304, num_warps=8, num_stages=2),
     ConfigKey(M=  32768, K=16384, N= 6144, G= 8): Config(block_size_m=32, block_size_k=128, block_size_n=256, group_size=4, grid_dim=304, num_warps=8, num_stages=2),
     # TN
-    # TODO: Tune TN persistent TGMM.
-    ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_lhs=True): DEFAULT_PERSISTENT_TGMM_CONFIG,
-    ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_lhs=True): DEFAULT_PERSISTENT_TGMM_CONFIG,
-    ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_lhs=True): DEFAULT_PERSISTENT_TGMM_CONFIG,
-    ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True): DEFAULT_PERSISTENT_TGMM_CONFIG,
-    ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True): DEFAULT_PERSISTENT_TGMM_CONFIG,
+    ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_lhs=True): Config(block_size_m=32, block_size_k=128, block_size_n=256, group_size=4, grid_dim=304, num_warps=8, num_stages=2),
+    ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_lhs=True): Config(block_size_m=64, block_size_k=128, block_size_n=128, group_size=4, grid_dim=608, num_warps=2, num_stages=1),
+    ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_lhs=True): Config(block_size_m=32, block_size_k=256, block_size_n=256, group_size=2, grid_dim=912, num_warps=8, num_stages=2),
+    ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True): Config(block_size_m=32, block_size_k=256, block_size_n=256, group_size=4, grid_dim=912, num_warps=8, num_stages=2),
+    ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True): Config(block_size_m=32, block_size_k=256, block_size_n=256, group_size=4, grid_dim=304, num_warps=8, num_stages=2),
 }
 # fmt: on
 
@@ -174,12 +172,11 @@ BEST_NON_PERSISTENT_TGMM_CONFIGS: dict[ConfigKey, Config] = {
     ConfigKey(M=  32768, K= 6144, N=16384, G= 8): Config(grid_dim=None, block_size_m=32, block_size_k=128, block_size_n=256, group_size=2, num_warps=8, num_stages=2),
     ConfigKey(M=  32768, K=16384, N= 6144, G= 8): Config(grid_dim=None, block_size_m=32, block_size_k=256, block_size_n=256, group_size=1, num_warps=8, num_stages=2),
     # TN
-    # TODO: Tune TN non-persistent TGMM.
-    ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_lhs=True): DEFAULT_NON_PERSISTENT_TGMM_CONFIG,
-    ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_lhs=True): DEFAULT_NON_PERSISTENT_TGMM_CONFIG,
-    ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_lhs=True): DEFAULT_NON_PERSISTENT_TGMM_CONFIG,
-    ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True): DEFAULT_NON_PERSISTENT_TGMM_CONFIG,
-    ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True): DEFAULT_NON_PERSISTENT_TGMM_CONFIG,
+    ConfigKey(M=  49152, K= 1408, N= 2048, G=64, trans_lhs=True): Config(grid_dim=None, block_size_m= 32, block_size_k=256, block_size_n=256, group_size=8, num_warps=8, num_stages=2),
+    ConfigKey(M=3145728, K= 2048, N= 1408, G= 8, trans_lhs=True): Config(grid_dim=None, block_size_m=128, block_size_k=256, block_size_n=128, group_size=1, num_warps=8, num_stages=1),
+    ConfigKey(M= 393216, K= 2048, N= 1408, G=64, trans_lhs=True): Config(grid_dim=None, block_size_m= 32, block_size_k=256, block_size_n=256, group_size=2, num_warps=8, num_stages=2),
+    ConfigKey(M=  32768, K= 6144, N=16384, G= 8, trans_lhs=True): Config(grid_dim=None, block_size_m= 32, block_size_k=256, block_size_n=256, group_size=1, num_warps=8, num_stages=2),
+    ConfigKey(M=  32768, K=16384, N= 6144, G= 8, trans_lhs=True): Config(grid_dim=None, block_size_m= 32, block_size_k=256, block_size_n=256, group_size=1, num_warps=8, num_stages=2),
 }
 # fmt: on
 
