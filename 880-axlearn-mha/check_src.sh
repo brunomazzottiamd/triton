ab65b5e95587e14f77bf76df73cfa9c8ebdfcecd
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-06-20 14:29:26 +0000

    add check_src.sh script

---
+++ 880-axlearn-mha/check_src.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+black "${script_dir}"/*.py
+ruff check "${script_dir}"/*.py
+
+# mypy has issues with Triton on Windows, run it only on Linux.
+if [ "$(uname)" == Linux ]; then
+    mypy --ignore-missing-imports "${script_dir}"/*.py
+fi
+
+if [ "$(uname)" == Linux ]; then
+    shellcheck "${script_dir}"/*.sh
+else
+    # Ignore literal carriage return warnings on Windows.
+    dos2unix --quiet "${script_dir}"/*.sh
+    shellcheck --exclude SC1017 "${script_dir}"/*.sh
+    unix2dos --quiet "${script_dir}"/*.sh
+fi
