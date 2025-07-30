b970b131516aa29639059532db98da1002a9fbda
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-07-30 16:33:52 -0300

    improve Triton common module

--- 1061_mojo_triton/triton/array_ser.py
+++ 1061_mojo_triton/triton/array_ser.py
@@ -1,28 +1,19 @@
-import os
-
-import numpy as np
-
-from common import gen_tensor
+from common import gen_tensor, save_tensor, load_tensor, tensor_equal
 
 
 # Generate Triton array.
 array_size = 10
 triton_array = gen_tensor(array_size)
 
-# Get directory of serialized arrays.
-script_path = os.path.abspath(__file__)
-script_dir = os.path.dirname(script_path)
-arrays_dir = os.path.dirname(script_dir)
-
 # Save Triton array.
-triton_array_file = os.path.join(arrays_dir, "triton_array.npz")
-np.savez(triton_array_file, triton_array=triton_array)
+save_tensor("triton_array", triton_array)
 
 # Load Mojo array and compare with Triton array.
-mojo_array_file = os.path.join(arrays_dir, "mojo_array.npz")
-if os.path.exists(mojo_array_file):
-    mojo_array = np.load(mojo_array_file)["mojo_array"]
-    equal = np.array_equal(triton_array, mojo_array)
-    print("Triton array matches Mojo array?", "Yes" if equal else "No")
+mojo_array = load_tensor("mojo_array")
+if mojo_array is not None:
+    print(
+        "Triton array matches Mojo array?",
+        "Yes" if tensor_equal(triton_array, mojo_array) else "No",
+    )
 else:
     print("Couldn't find Mojo array file.")
--- 1061_mojo_triton/triton/common.py
+++ 1061_mojo_triton/triton/common.py
@@ -1,17 +1,50 @@
+import os
+
 import numpy as np
 
 import torch
 
 
+DEFAULT_RNG_SEED: int = 20250730
+
+
 def gen_tensor(
     shape: int | tuple[int, ...],
-    rng_seed: int | None = 20250730,
+    rng_seed: int | None = DEFAULT_RNG_SEED,
 ) -> np.ndarray:
     if rng_seed is not None:
         np.random.seed(rng_seed)
     if isinstance(shape, int):
         shape = (shape,)
     return np.random.randn(*shape).astype(np.float16)
+
+
+def tensor_equal(x: np.ndarray, y: np.ndarray) -> bool:
+    return np.array_equal(x, y)
+
+
+def tensors_dir() -> str:
+    triton_source_dir = os.path.dirname(os.path.abspath(__file__))
+    project_root_dir = os.path.dirname(triton_source_dir)
+    tensors_dir = os.path.join(project_root_dir, "tensors")
+    if not os.path.exists(tensors_dir):
+        os.mkdir(tensors_dir)
+    return tensors_dir
+
+
+def tensor_file(tensor_name: str) -> str:
+    return f"{tensors_dir()}/{tensor_name}.npz"
+
+
+def save_tensor(tensor_name: str, x: np.ndarray):
+    np.savez_compressed(tensor_file(tensor_name), x)
+
+
+def load_tensor(tensor_name: str) -> np.ndarray | None:
+    f = tensor_file(tensor_name)
+    if not os.path.exists(f):
+        return None
+    return np.load(f)["arr_0"]
 
 
 def np_to_torch(x: np.ndarray) -> torch.Tensor:
