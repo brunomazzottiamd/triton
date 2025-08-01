f1ae8d127bc855276813051442a12d6c7af522a7
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-08-01 20:59:45 +0000

    rename triton directory to python

--- 1061_mojo_triton/array_ser.sh
+++ 1061_mojo_triton/array_ser.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-triton_dir="${script_dir}/triton"
+python_dir="${script_dir}/python"
 mojo_dir="${script_dir}/mojo"
 
-python "${triton_dir}/array_ser.py"
+python "${python_dir}/array_ser.py"
 
 pixi run --manifest-path="${mojo_dir}/pixi.toml" \
     mojo run "${mojo_dir}/array_ser.mojo"
