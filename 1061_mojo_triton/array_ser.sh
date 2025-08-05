94007bda63e671a0502d86cf183200c279ae488a
Bruno Mazzotti <bruno.mazzotti@amd.com>
2025-08-05 14:32:37 +0000

    use common shell library in array serialization test

--- 1061_mojo_triton/array_ser.sh
+++ 1061_mojo_triton/array_ser.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-python_dir="${script_dir}/python"
-mojo_dir="${script_dir}/mojo"
-tensors_dir="${script_dir}/tensors"
+source "${script_dir}/common.sh"
 
-rm --force --recursive "${tensors_dir}/"*array_ser*
+clean_tensors 'array_ser'
 
 echo 'Running Triton array serialization...'
-python "${python_dir}/array_ser.py"
+run_python 'array_ser'
 
 echo 'Running Mojo array serialization...'
-pixi run --manifest-path="${mojo_dir}/pixi.toml" \
-    mojo run "${mojo_dir}/array_ser.mojo"
+run_mojo 'array_ser'
 
 echo 'Running Triton array serialization again...'
-python "${python_dir}/array_ser.py"
+run_python 'array_ser'
+
+clean_tensors 'array_ser'
